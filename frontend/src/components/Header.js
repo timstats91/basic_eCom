0a1b5bb6682db761da4947d27ece084ebebf7593
timstats91 <timstats91@gmail.com>
2021-12-17 16:52:43 -0600

    Adding header and footer components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Nav, Navbar, Container } from 'react-bootstrap'
+
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="ml-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart </Nav.Link>
+                        <Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
