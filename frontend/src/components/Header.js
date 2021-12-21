aa1441725f12dd93025153a1cfe273248a215a43
timstats91 <timstats91@gmail.com>
2021-12-21 08:36:47 -0600

    Getting started with React Router v6

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Nav, Navbar, Container } from 'react-bootstrap'
 
 
@@ -7,7 +8,9 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -15,8 +18,12 @@ const Header = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart </Nav.Link>
-                        <Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
